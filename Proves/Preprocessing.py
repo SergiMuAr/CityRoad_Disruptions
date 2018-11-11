5e1fa7ece78f585c7a166dd66faebe2beb1cb185
SergiMuAr <sergimunoz.a@gmail.com>
2018-11-11 01:32:39 +0100

    added lemmatizer to test

--- Proves/Preprocessing.py
+++ Proves/Preprocessing.py
@@ -1,17 +1,14 @@
 import csv
 import string
 import nltk
-import re
+import stop_words
 from nltk import sent_tokenize
 from nltk.tokenize.toktok import ToktokTokenizer
-from nltk.tokenize import TweetTokenizer
 
-# Open/create a file to append data to
-csvFile = open('resultsPreprocess.csv', 'a')
-csvWriter = csv.writer(csvFile)
 
 with open('resultsTransitComplete1.csv') as f:
   reader = csv.reader(f)
+  i = 10
   for row in reader:
     # print (row[1])
     # sentences = sent_tokenize(row[1])
@@ -19,24 +16,14 @@ with open('resultsTransitComplete1.csv') as f:
     # print "ESPAAAAAAI"
 
     # Tokenize tweets. Word splitting.
-    # from nltk.tokenize.toktok import ToktokTokenizer
+    from nltk.tokenize.toktok import ToktokTokenizer
     toktok = ToktokTokenizer()
     tokens = toktok.tokenize(row)
     table = str.maketrans('', '', string.punctuation)
     words = [w.translate(table) for w in tokens]
 
-    # Remove hyperlinks
-    list_no_hyperlinks=[re.sub(r'https?:\/\/.*\/\w*','',i) for i in words]
-    # print('No hyperlinks:')
-    # print(list_no_hyperlinks)
-    # Remove hashtags
-    list_no_hashtags=[re.sub(r'#', '', i) for i in list_no_hyperlinks]
-    # print('No hashtags:')
-    # print(list_no_hashtags)
-
-
     # Filter only if word is alphabetic characters only. (fer que AP-7 també estigui acceptat -> guions i numeros)
-    words = [word for word in list_no_hashtags if word.isalnum()]
+    words = [word for word in words if word.isalnum()]
     # print ("AIXÒ SÓN WORDS FINALS:")
     # words (words)
     #convert to lowercase
@@ -47,20 +34,46 @@ with open('resultsTransitComplete1.csv') as f:
     # hem agafat els stop_words de http://latel.upf.edu/morgana/altres/pub/ca_stop.htm (ens hem fet la nostra propia funció)
     stop_words = get_stopwords()
     words = [w for w in words if not w in stop_words]
-    print (words)
+
     # Stemming
     # from nltk.stem.porter import PorterStemmer
     # porter = PorterStemmer()
     # stemmed = [porter.stem(word) for word in words]
-    # csvWriter.writerow(stemmed)
-    # print (w for w in words)
-    # print (words)
+    # print(stemmed)
 
-    # en comptes de run hauré de fer un open-wait perque sino estic obrint i tancant procés per cada paraula.
-    from subprocess import run, PIPE
-    for w in words:  
-      p = run(['/home/sergi/snowball/stemwords', '-l', 'catalan'], stdout=PIPE,
-              input=w, encoding='utf-8')
-      print(p.stdout)
+    # import subprocess
+    # args = ("./stemwords", "-l", "cat", "-i", "input_file.txt", "-o", "output_file.txt")
+    # popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    # popen.wait()
 
-csvFile.close()   
+    # For "unixy" systems like linux and OSX, the pexpect module is written to handle the complexities of an interactive child process. 
+    # For Windows, there is no good tool that I know of to do it.
+    # import subprocess
+    # p = subprocess.Popen(['/home/sergi/snowball/stemwords', '-l', 'catalan'],
+    #                   stdin=subprocess.PIPE,
+    #                   stdout=subprocess.PIPE,
+    #                   universal_newlines=True)
+    # for w in words:
+    #   stdout_data, stderr_data = p.communicate(''.join(w))
+    #   print(stdout_data)
+    #   p.wait()
+
+
+    # LEMMATIZER
+    #encoding: utf8
+    # with open('lemmatization-es.txt', 'rb') as f:
+    #   data = f.read().decode('utf8').replace(u'\r', u'').split(u'\n')
+    #   data = [a.split(u'\t') for a in data]
+      
+    # lemmaDict = { a[1]:a[0] for a in data if len(a)>1 }
+    # lemmaDict.update( { a:a for a in set(lemmaDict.values()) } )
+      
+    # def lemmatize(word):
+    #   return lemmaDict.get(word, word + u'*')
+      
+    # def test():
+    #   for a in [ u'salió', u'usuarios', u'abofeteéis', u'diferenciando', u'diferenciándola' ]:
+    #       print(lemmatize(a))
+    
+    if (i<0): break
+    else: i = i-1
