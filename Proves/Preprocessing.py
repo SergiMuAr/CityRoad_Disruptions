a271367e4bed9761f053e11a0d6c43cac01fbb01
SergiMuAr <sergimunoz.a@gmail.com>
2018-11-20 20:57:03 +0100

    delete emoji

--- Proves/Preprocessing.py
+++ Proves/Preprocessing.py
@@ -76,6 +76,19 @@ with open('TrainingDataSet/trainingDataSet.csv') as f:
     #   for a in [ u'salió', u'usuarios', u'abofeteéis', u'diferenciando', u'diferenciándola' ]:
     #       print(lemmatize(a))
     
+    # Treure ascii
+    # words = [w.encode('ascii', 'ignore').decode('ascii') for w in words]
+    import re
+    emoji_pattern = re.compile("["
+                        u"\U0001F600-\U0001F64F"  # emoticons
+                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                        u"\U00002702-\U000027B0"
+                        u"\U000024C2-\U0001F251"
+                        "]+", flags=re.UNICODE)
+    words = [emoji_pattern.sub(r'', w) for w in words] # no emoji
+
     csvWriter.writerow(words)
    
 csvFile.close()
