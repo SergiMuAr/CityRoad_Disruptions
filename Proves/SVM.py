aa65c5b0fde7f8788a4c974c3efed8d17c78e696
SergiMuAr <sergimunoz.a@gmail.com>
2018-12-29 14:39:54 +0100

    try for SVM

--- Proves/SVM.py
+++ Proves/SVM.py
@@ -1,33 +1,84 @@
+#Import Library
+from sklearn import svm
+import csv
 import numpy as np
-import matplolib.pyplot as plt
-from sklearn import svm, datasets
+import pandas as pd
 
-# import some data to play with
-iris = datasets.load_iris()
-X = iris.data[:, :2] # we only take the first two features. We could
- # avoid this ugly slicing by using a two-dim dataset
-y = iris.target
+data = pd.read_csv('TrainingDataSet/trainingBO.csv', sep = '\t', lineterminator='\n')
+text = data['Text'] #you can also use df['column_name']
+target = data['Class']
 
-# we create an instance of SVM and fit out data. We do not scale our
-# data since we want to plot the support vectors
-C = 1.0 # SVM regularization parameter
-svc = svm.SVC(kernel='linear', C=1,gamma='auto').fit(X, y)
+# labels, texts = [], []
+# for i, line in enumerate(data.split("\n")):
+#     content = line.split()
+#     labels.append(content[0])
+#     texts.append(" ".join(content[1:]))
 
-# create a mesh to plot in
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-h = (x_max / x_min)/100
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-np.arange(y_min, y_max, h))
+# create a dataframe using texts and lables
+# trainDF = pandas.DataFrame()
+# trainDF['text'] = texts
+# trainDF['label'] = labels
+# tlist = []
+# with open('TrainingDataSet/stemmedNTI.csv') as f:
+#   reader = csv.reader(f)
+#   for row in reader:
+#     tlist.append(','.join(row))
 
-plt.subplot(1, 1, 1)
-Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+# print (tlist[0])
 
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
-plt.xlim(xx.min(), xx.max())
-plt.title('SVC with linear kernel')
-plt.show()
+# test = []
+# with open('JocsDeProves/testPreprocess.csv') as f:
+#   reader = csv.reader(f)
+
+#   for row in reader:
+#         test.append(','.join(row))
+
+
+#Assumed you have, X (predictor) and Y (target) for training data set and x_test(predictor) of test_dataset
+# Create SVM classification object 
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+# tf_vectorizer = CountVectorizer(max_df=0.95, min_df=5, max_features=100)
+# tf = tf_vectorizer.fit_transform(tlist)
+# tf_feature_names = tf_vectorizer.get_feature_names()
+
+# print ("FEATURES")
+# print (tf)
+# print (tf_feature_names)
+
+tf_vectorizer = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2))
+train_matrix = tf_vectorizer.fit_transform(text).toarray()
+# test_matrix = tf_vectorizer.transform(test)
+
+# labels = df.category_id
+
+from sklearn.feature_selection import chi2
+print (train_matrix.shape) # 445 tweets are represented by 387 features.
+
+model = svm.SVC(kernel='linear', gamma=1) 
+# there is various option associated with it, like changing kernel, gamma and C value. Will discuss more # about it in next section.Train the model using the training sets and check score
+model.fit(train_matrix, target)
+print (model.score(train_matrix, target))
+
+
+# N = 2
+# for Product, category_id in sorted(category_to_id.items()):
+#   features_chi2 = chi2(features, labels == category_id)
+#   indices = np.argsort(features_chi2[0])
+#   feature_names = np.array(tfidf.get_feature_names())[indices]
+#   unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
+#   bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
+#   print("# '{}':".format(Product))
+#   print("  . Most correlated unigrams:\n. {}".format('\n. '.join(unigrams[-N:])))
+#   print("  . Most correlated bigrams:\n. {}".format('\n. '.join(bigrams[-N:])))
+
+
+# X = [[0], [1], [2], [3]]
+# Y = [0, 1, 2, 3]
+
+# model = svm.SVC(kernel='linear', gamma=1) 
+# # there is various option associated with it, like changing kernel, gamma and C value. Will discuss more # about it in next section.Train the model using the training sets and check score
+# model.fit(X, Y)
+# model.score(X, Y)
+# #Predict Output
+# print (model.predict([2]))
